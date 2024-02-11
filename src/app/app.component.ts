ca0c8ae95b22baf831424ed472eed8a3a598562b
JARA-17 <juandres435@gmail.com>
2024-02-11 11:43:33 +0100

    Create a user component for the activity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  username: string = '';
-
-  resetUsername(): void {
-    this.username = '';
-  }
-}
+export class AppComponent {}
